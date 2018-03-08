8e7445c2c5c59ae99780ecc7ff82db2ea0546d55
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-08 09:58:49 +0300

    Initial commit

---
+++ index.view.php
@@ -0,0 +1,49 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+        <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        
+        <nav>
+          <div class="nav-wrapper">
+            <a href="/" class="brand-logo">JM</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="#">Github</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="container">
+          
+          <table>
+            <tbody>
+             <tr>
+               <th>Operating System</th>
+               <td></td>
+             </tr>
+             <tr>
+               <th>Web browser</th>
+               <td></td>
+             </tr>
+             <tr>
+               <th>IP address</th>
+               <td></td>
+             </tr>
+            </tbody>
+          </table>
+
+        </div>      
+      
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    </body>
+  </html>
